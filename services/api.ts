65e9914f7e6191c9cb8dec1ceb6ec1995b02a59c
GeoDash123 <josealatorre233@gmail.com>
2026-07-14 23:50:36 -0700

    Cliente-servidor inicial

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://172.25.160.1:5678";
+const API_URL = "http://192.168.1.103:5678";
 
 export const api = axios.create({
     baseURL: API_URL,
--- services/expenseService.ts
+++ services/expenseService.ts
@@ -2,7 +2,7 @@ import { api } from "./api";
 import { Expense } from "../types/Expense";
 
 export async function createExpense(expense: Expense) {
-    const response = await api.post("/webhook/walletia", expense);
+    const response = await api.post("/webhook-test/walletia", expense);
 
     return response.data;
 }
